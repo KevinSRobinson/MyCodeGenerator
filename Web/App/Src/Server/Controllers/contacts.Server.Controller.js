608f4df4b716e63dff590c64c324a0b035609492
Kevin Robinson <kevin.robinson@outlook.com>
2017-01-03 14:58:59 +0000

    wer

--- Web/App/Src/Server/Controllers/contacts.Server.Controller.js
+++ Web/App/Src/Server/Controllers/contacts.Server.Controller.js
@@ -1,31 +1,40 @@
-var Contact = require('../Models/contactsModel.js');
 
-exports.create = function (req, res) {
+    
+    var Contact = require('../Models/contactsModel.js');
 
-    var entry = new Contact({
-        Id: req.body.Id,
-        FirstName: req.body.FirstName,
-        LastName: req.body.LastName,
-        UserId: req.body.UserId
-    });
+    exports.create = function (req, res) {
+        var entry = new Contact({
+            
+            Id: req.body.Id,FirstName: req.body.FirstName,LastName: req.body.LastName,UserId: req.body.UserId
+          
 
-    entry.save();
-
-    res.redirect(301, '/');
-};
-
-exports.list = function (req, res) {
-    var query = Contact.find({}).then(function (results) {
-        res.json({
-            data: results
+            
         });
-    });
-};
 
-exports.findById = function (req, res) {
+        entry.save();
+
+        res.redirect(301, '/');
+    };
+
+
+    exports.list = function (req, res) { 
+         var query = Contact.find({}).then(function (results) {
+         res.json({data: results});
+    });
+
+
+    };
+
+
+
+   exports.findById = function (req, res) {
+    console.log(req.params.id);
     var query = Contact.findById(req.params.id).then(function (results) {
         res.json({
             data: results
         });
     });
 };
+
+ 
+
